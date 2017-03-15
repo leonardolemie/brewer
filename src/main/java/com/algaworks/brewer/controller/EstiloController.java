8327007a2a2ae729b6c7b954e697d0299527c01b
Leonardo Bernardes <leonardolemie@gmail.com>
2017-03-14 21:55:08 -0300

    Added ControllerAdvice to handle exception

--- src/main/java/com/algaworks/brewer/controller/EstiloController.java
+++ src/main/java/com/algaworks/brewer/controller/EstiloController.java
@@ -20,17 +20,18 @@ import com.algaworks.brewer.service.CadastroEstiloService;
 import com.algaworks.brewer.service.exception.NomeEstiloJaCadastradoException;
 
 @Controller
+@RequestMapping("/estilos")
 public class EstiloController {
 	
 	@Autowired
 	CadastroEstiloService estiloService;
 
-	@RequestMapping("/estilos/novo")
+	@RequestMapping("/novo")
 	public ModelAndView novo(Estilo estilo) {
 		return new ModelAndView("estilo/CadastroEstilo");
 	}
 	
-	@RequestMapping(value = "/estilos/novo", method = RequestMethod.POST)
+	@RequestMapping(value = "/novo", method = RequestMethod.POST)
 	public ModelAndView cadastrar(@Valid Estilo estilo, BindingResult result, Model model, RedirectAttributes attributes){
 		if (result.hasErrors()){
 			return novo(estilo);
@@ -47,17 +48,13 @@ public class EstiloController {
 		return new ModelAndView("redirect:/estilos/novo");
 	}
 	
-	@RequestMapping(value = "/estilos", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
+	@RequestMapping(method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
 	public @ResponseBody ResponseEntity<?> salvar(@RequestBody @Valid Estilo estilo, BindingResult result){
 		if (result.hasErrors()){
 			return ResponseEntity.badRequest().body(result.getFieldError("nome").getDefaultMessage());
 		}
 		
-		try {
-			estilo = estiloService.salvar(estilo);
-		} catch (NomeEstiloJaCadastradoException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
+		estilo = estiloService.salvar(estilo);
 		
 		return ResponseEntity.ok(estilo);
 		
